aaa3f8f151a815c429fe95cc5c2ade4cd61ab881
Phillip Burt <pab37@students.uwf.edu>
2020-02-07 15:03:52 -0600

    working on a fragment, model control viewer model

---
+++ app/src/main/java/com/example/argohunt/SingleFragmentActivity.kt
@@ -0,0 +1,24 @@
+package com.example.argohunt
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+abstract class SingleFragmentActivity : AppCompatActivity()
+{
+    protected abstract fun createFragment(): Fragment?
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment)
+        val fm = supportFragmentManager
+        var fragment = fm.findFragmentById(R.id.fragment_container)
+        if (fragment == null) {
+            fragment = createFragment()
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment!!)
+                    .commit()
+        }
+    }
+}
